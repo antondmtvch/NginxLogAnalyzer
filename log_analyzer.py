84dfee9e6cbd8bdf82065aa6d85e2a898e4b3be7
ateplyakov <antondmtvch@gmail.com>
2020-09-08 16:27:07 +0300

    Append log file path.

--- log_analyzer.py
+++ log_analyzer.py
@@ -2,8 +2,8 @@ import os
 import sys
 import configparser
 import argparse
+import logging
 from log_analyzer.parser import Parser
-
 
 config = {
     "REPORT_SIZE": 1000,
@@ -12,11 +12,11 @@ config = {
 }
 
 
-def config_parser(path: str) -> dict:
-    conf = configparser.ConfigParser()
-    conf.optionxform = str
-    conf.read(path)
-    return conf['CONFIG']
+def set_logging_filename(filename=None) -> None:
+    logging.basicConfig(
+        format='[%(asctime)s] %(levelname).1s %(message)s',
+        level=logging.INFO, datefmt='%Y.%m.%d %H:%M:%S', filename=filename
+    )
 
 
 def argument_parser() -> argparse.Namespace:
@@ -24,27 +24,33 @@ def argument_parser() -> argparse.Namespace:
     parser.add_argument('--config', dest='config', type=str,
                         default=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'settings.ini'),
                         help='Путь к файлу с конфигом.')
-
-    args = parser.parse_args()
-
-    if not os.path.exists(args.config):
-        print(f'Файл {args.config} не существует.')
-        sys.exit(1)
-    return args
+    return parser.parse_args()
 
 
 def update_default_config(default: dict) -> None:
     args = argument_parser()
-    other = config_parser(args.config)
+    parser = configparser.ConfigParser()
+    parser.optionxform = str
+
+    with open(args.config, 'r', encoding='utf-8') as f:
+        parser.read_file(f)
+
+    other = parser['CONFIG'] if 'CONFIG' in parser.sections() else {}
     default.update(other)
+    set_logging_filename(default.get('LOG_FILE'))
 
 
-def main() -> None:
-    update_default_config(config)
-    parser = Parser()
-    parser.parse(log_dir=config['LOG_DIR'], report_dir=config['REPORT_DIR'])
-    parser.generate_report(report_size=config['REPORT_SIZE'])
+def main(_config: dict) -> None:
+    try:
+        update_default_config(_config)
+        parser = Parser()
+        parser.parse(log_dir=_config['LOG_DIR'], report_dir=_config['REPORT_DIR'])
+        parser.generate_report(report_size=_config['REPORT_SIZE'])
+    except Exception as e:
+        logging.exception(e)
+        sys.exit(1)
 
 
 if __name__ == "__main__":
-    main()
+    main(config)
+
